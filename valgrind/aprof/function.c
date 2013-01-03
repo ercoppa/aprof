f3434f7864ec4bf79e1cbf892ef59e9ab4e7220f
ercoppa <ercoppa@gmail.com>
2013-01-03 11:56:59 +0000

    [aprof:dev] merged external I/O on events.c function.c CCT.c fwrite.c debug.c

--- valgrind/aprof/function.c
+++ valgrind/aprof/function.c
@@ -10,7 +10,8 @@
 
    Copyright (C) 2011-2012, Emilio Coppa (ercoppa@gmail.com),
                             Camil Demetrescu,
-                            Irene Finocchi
+                            Irene Finocchi,
+                            Romolo Marotta
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -33,405 +34,374 @@
 #include "aprof.h"
 
 void APROF_(destroy_routine_info)(RoutineInfo * ri) {
-	
-	#if CCT
-	HT_destruct(ri->context_rms_map);
-	#else
-	HT_destruct(ri->rms_map);
-	#endif
-	
-	VG_(free)(ri);
+    
+    #if CCT
+    HT_destruct(ri->context_rms_map);
+    #else
+    HT_destruct(ri->rms_map);
+    #endif
+    
+    VG_(free)(ri);
 
 }
 
 RoutineInfo * APROF_(new_routine_info)(ThreadData * tdata, 
-								Function * fn, UWord target) {
-	
-	#if DEBUG
-	AP_ASSERT(tdata != NULL, "Thread data is not valid");
-	AP_ASSERT(fn != NULL, "Invalid function info");
-	AP_ASSERT(target > 0, "Invalid target");
-	#endif
-	
-	RoutineInfo * rtn_info = VG_(calloc)("rtn_info", 1, sizeof(RoutineInfo));
-	#if DEBUG
-	AP_ASSERT(rtn_info != NULL, "rtn info not allocable");
-	#endif
-	
-	#if DEBUG_ALLOCATION
-	APROF_(add_alloc)(RTS);
-	#endif
-	
-	rtn_info->key = target;
-	rtn_info->fn = fn;
-	
-	/*
-	rtn_info->total_self_time = 0;
-	rtn_info->total_cumulative_time = 0;
-	rtn_info->calls = 0;
-	rtn_info->recursive = 0;
-	rtn_info->recursion_pending = 0;
-	*/
-	
-	#if DISCARD_UNKNOWN
-	if (!rtn_info->fn->discard_info) {
-	#endif
-	
-	rtn_info->routine_id = tdata->next_routine_id++;
+                                Function * fn, UWord target) {
+    
+    #if DEBUG
+    AP_ASSERT(tdata != NULL, "Thread data is not valid");
+    AP_ASSERT(fn != NULL, "Invalid function info");
+    AP_ASSERT(target > 0, "Invalid target");
+    #endif
+    
+    RoutineInfo * rtn_info = VG_(calloc)("rtn_info", 1, sizeof(RoutineInfo));
+    #if DEBUG
+    AP_ASSERT(rtn_info != NULL, "rtn info not allocable");
+    #endif
+    
+    #if DEBUG_ALLOCATION
+    APROF_(add_alloc)(RTS);
+    #endif
+    
+    rtn_info->key = target;
+    rtn_info->fn = fn;
+    
+    /*
+    rtn_info->total_self_time = 0;
+    rtn_info->total_cumulative_time = 0;
+    rtn_info->calls = 0;
+    rtn_info->recursive = 0;
+    rtn_info->recursion_pending = 0;
+    */
+    
+    #if DISCARD_UNKNOWN
+    if (!rtn_info->fn->discard_info) {
+    #endif
+    
+    rtn_info->routine_id = tdata->next_routine_id++;
 
-	#if CCT
-	
-	/* elements of this ht are freed when we generate the report */
-	rtn_info->context_rms_map = HT_construct(NULL);
-	
-	#if DEBUG
-	AP_ASSERT(rtn_info->context_rms_map != NULL, "context_sms_map not allocable");
-	#endif
-	
-	#if DEBUG_ALLOCATION
-	 APROF_(add_alloc)(HT);
-	#endif
-	
-	#else
-	
-	/* elements of this ht are freed when we generate the report */
-	rtn_info->rms_map = HT_construct(NULL);
-	#if DEBUG
-	AP_ASSERT(rtn_info->rms_map != NULL, "sms_map not allocable");
-	#endif
-	
-	#if DEBUG_ALLOCATION
-	APROF_(add_alloc)(HT);
-	#endif
-	
-	#endif
-	
-	#if DISCARD_UNKNOWN
-	}
-	#endif
-	
-	HT_add_node(tdata->routine_hash_table, rtn_info->key, rtn_info);
-	
-	#if DEBUG_ALLOCATION
-	APROF_(add_alloc)(HTN);
-	#endif
-	
-	return rtn_info;
+    #if CCT
+    
+    /* elements of this ht are freed when we generate the report */
+    rtn_info->context_rms_map = HT_construct(NULL);
+    
+    #if DEBUG
+    AP_ASSERT(rtn_info->context_rms_map != NULL, "context_sms_map not allocable");
+    #endif
+    
+    #if DEBUG_ALLOCATION
+     APROF_(add_alloc)(HT);
+    #endif
+    
+    #else
+    
+    /* elements of this ht are freed when we generate the report */
+    rtn_info->rms_map = HT_construct(NULL);
+    #if DEBUG
+    AP_ASSERT(rtn_info->rms_map != NULL, "sms_map not allocable");
+    #endif
+    
+    #if DEBUG_ALLOCATION
+    APROF_(add_alloc)(HT);
+    #endif
+    
+    #endif
+    
+    #if DISCARD_UNKNOWN
+    }
+    #endif
+    
+    HT_add_node(tdata->routine_hash_table, rtn_info->key, rtn_info);
+    
+    #if DEBUG_ALLOCATION
+    APROF_(add_alloc)(HTN);
+    #endif
+    
+    return rtn_info;
 }
 
 void APROF_(function_enter)(ThreadData * tdata, Activation * act) {
 
-	#if DEBUG
-	AP_ASSERT(tdata != NULL, "Thread data is not valid");
-	AP_ASSERT(act != NULL, "Invalid activation info");
-	#endif
+    #if DEBUG
+    AP_ASSERT(tdata != NULL, "Thread data is not valid");
+    AP_ASSERT(act != NULL, "Invalid activation info");
+    #endif
+    
+    #if VERBOSE
+    VG_(printf)("Enter: %s\n", act->rtn_info->fn->name);
+    /*
+    int i = 0;
+    for(i = 0; i < tdata->stack_depth - 1; i++)
+        VG_(printf)("| ");
+    */
+    //VG_(printf)("[%lu] %s\n", tdata->stack_depth, act->rtn_info->fn->name);
+    #endif
+    
+    #if EVENTCOUNT >= 2
+    tdata->num_func_enter++;
+    #endif
+    
+    #if EMPTY_ANALYSIS
+    return;
+    #endif
+    
+    ULong start = APROF_(time)(tdata);
 
-	#if 0
-	if (VG_(strcmp)(act->rtn_info->fn->name, "strcmp") == 0) {
-		inside_strcmp = 1;
-		VG_(printf)("\n\n");
-	}
-	#endif
-	
-	#if VERBOSE
-	VG_(printf)("Enter: %s\n", act->rtn_info->fn->name);
-	/*
-	int i = 0;
-	for(i = 0; i < tdata->stack_depth - 1; i++)
-		VG_(printf)("| ");
-	*/
-	//VG_(printf)("[%lu] %s\n", tdata->stack_depth, act->rtn_info->fn->name);
-	#endif
-	
-	#if EVENTCOUNT >= 2
-	tdata->num_func_enter++;
-	#endif
-	
-	#if EMPTY_ANALYSIS
-	return;
-	#endif
-	
-	ULong start = APROF_(time)(tdata);
+    RoutineInfo * rtn_info = act->rtn_info;
+    #if DEBUG
+    AP_ASSERT(rtn_info != NULL, "Invalid rtn info");
+    #endif
 
-	RoutineInfo * rtn_info = act->rtn_info;
-	#if DEBUG
-	AP_ASSERT(rtn_info != NULL, "Invalid rtn info");
-	#endif
+    rtn_info->recursion_pending++;
+    act->rtn_info            = rtn_info;
+    act->entry_time          = start;
+    act->rms                 = 0;
+    act->total_children_time = 0;
+    act->aid                 = ++APROF_(global_counter);
+    
+    /* check & fix timestamp overflow */
+    if (act->aid == 0) 
+        act->aid = APROF_(global_counter) = APROF_(overflow_handler)();
+    
+    #if DISCARD_UNKNOWN
+    if (!rtn_info->fn->discard_info) {
+    #endif
+    
+    #if CCT
+    
+    CCTNode * parent = APROF_(parent_CCT)(tdata);
+    #if DEBUG
+    AP_ASSERT(parent != NULL, "Invalid parent CCT");
+    #endif
+    
+    CCTNode * cnode = parent->firstChild;
+    
+    // did we already see this context?
+    while (cnode != NULL) {
+        
+        if (cnode->routine_id == act->rtn_info->routine_id) break;
+        cnode = cnode->nextSibling;
+    
+    }
 
-	rtn_info->recursion_pending++;
-	act->rtn_info            = rtn_info;
-	act->entry_time          = start;
-	act->rms                 = 0;
-	act->total_children_time = 0;
-	act->aid                 = tdata->next_aid++;
-	
-	/* check & fix timestamp overflow */
-	if (act->aid == 0) {
-		
-		//VG_(printf)("\nSUF compress\n");
-		
-		/* Collect all valid aid */
-		UInt * arr_aid = VG_(calloc)("arr rid", tdata->stack_depth - 1, sizeof(UInt));
-		int j = 0;
-		for (j = 0; j < tdata->stack_depth - 1; j++) {
-			Activation * act_c = APROF_(get_activation)(tdata, j + 1);
-			arr_aid[j] = act_c->aid;
-			act_c->aid = j + 1;
-			//VG_(printf)("Aid was %u, now is %u\n", arr_aid[j], j+1);
-		}
-		LK_compress(tdata->accesses, arr_aid, tdata->stack_depth -1);
-		VG_(free)(arr_aid);
-		
-		tdata->next_aid = tdata->stack_depth;
-		act->aid = tdata->next_aid++;
-		
-		//VG_(printf)("Current aid is %u\nNext aid is %u\n", act->aid, tdata->next_aid);
-		
-	}
-	
-	#if DISCARD_UNKNOWN
-	if (!rtn_info->fn->discard_info) {
-	#endif
-	
-	#if CCT
-	
-	CCTNode * parent = APROF_(parent_CCT)(tdata);
-	#if DEBUG
-	AP_ASSERT(parent != NULL, "Invalid parent CCT");
-	#endif
-	
-	CCTNode * cnode = parent->firstChild;
-	
-	// did we already see this context?
-	while (cnode != NULL) {
-		
-		if (cnode->routine_id == act->rtn_info->routine_id) break;
-		cnode = cnode->nextSibling;
-	
-	}
+    if (cnode == NULL) {
+        
+        // create new context node
+        cnode = (CCTNode*) VG_(calloc)("CCT", sizeof(CCTNode), 1);
+        #if DEBUG
+        AP_ASSERT(cnode != NULL, "Can't allocate CTT node");
+        #endif
 
-	if (cnode == NULL) {
-		
-		// create new context node
-		cnode = (CCTNode*) VG_(calloc)("CCT", sizeof(CCTNode), 1);
-		#if DEBUG
-		AP_ASSERT(cnode != NULL, "Can't allocate CTT node");
-		#endif
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(CCTS);
+        #endif
 
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(CCTS);
-		#endif
+        // add node to tree
+        cnode->nextSibling = parent->firstChild;
+        parent->firstChild = cnode;
+        #if CCT_GRAPHIC
+        cnode->name = act->rtn_info->fn->name;
+        #endif
+        cnode->routine_id = act->rtn_info->routine_id;
+        cnode->context_id = tdata->next_context_id++;
+        
+        #if CCT_GRAPHIC
+        cnode->name = act->rtn_info->fn->name;
+        #endif
+    }
 
-		// add node to tree
-		cnode->nextSibling = parent->firstChild;
-		parent->firstChild = cnode;
-		#if CCT_GRAPHIC
-		cnode->name = act->rtn_info->fn->name;
-		#endif
-		cnode->routine_id = act->rtn_info->routine_id;
-		cnode->context_id = tdata->next_context_id++;
-		
-		#if CCT_GRAPHIC
-		cnode->name = act->rtn_info->fn->name;
-		#endif
-	}
-
-	// store context node into current activation record
-	act->node = cnode;
-	#endif
-	
-	#if DISCARD_UNKNOWN
-	}
-	#endif
-	
-	#if TRACE_FUNCTION
-	if (act->skip) tdata->skip = True;
-	#endif
-	
+    // store context node into current activation record
+    act->node = cnode;
+    #endif
+    
+    #if DISCARD_UNKNOWN
+    }
+    #endif
+    
+    #if TRACE_FUNCTION
+    if (act->skip) tdata->skip = True;
+    #endif
+    
 }
 
 void APROF_(function_exit)(ThreadData * tdata, Activation * act) {
-	
-	#if DEBUG
-	AP_ASSERT(tdata != NULL, "Thread data is not valid");
-	AP_ASSERT(act != NULL, "Invalid activation info");
-	#endif
-	
-	#if 0
-	if (VG_(strcmp)(act->rtn_info->fn->name, "strcmp") == 0) {
-		VG_(printf)("RMS: %d#\n\n", act->rms);
-		inside_strcmp = 0;
-	}
-	#endif
-	
-	#if VERBOSE
-	VG_(printf)("Exit: %s\n", act->rtn_info->fn->name);
-	#endif
-	
-	#if EVENTCOUNT >= 2
-	tdata->num_func_exit++;
-	#endif
-	
-	#if EMPTY_ANALYSIS
-	return;
-	#endif
+    
+    #if DEBUG
+    AP_ASSERT(tdata != NULL, "Thread data is not valid");
+    AP_ASSERT(act != NULL, "Invalid activation info");
+    #endif
+    
+    #if VERBOSE
+    VG_(printf)("Exit: %s\n", act->rtn_info->fn->name);
+    #endif
+    
+    #if EVENTCOUNT >= 2
+    tdata->num_func_exit++;
+    #endif
+    
+    #if EMPTY_ANALYSIS
+    return;
+    #endif
 
-	ULong start = APROF_(time)(tdata);
-	RoutineInfo * rtn_info = act->rtn_info;
+    ULong start = APROF_(time)(tdata);
+    RoutineInfo * rtn_info = act->rtn_info;
 
-	ULong partial_cumulative = start - act->entry_time;
-	
-	#if DISCARD_UNKNOWN
-	if (!rtn_info->fn->discard_info) {
-	#endif
-	
-	// check if routine has ever been called with this RMS
-	RMSInfo * info_access = NULL;
-	#if CCT
-	HashTable * rms_map  = HT_lookup(rtn_info->context_rms_map, 
-											act->node->context_id);
-	
-	if (rms_map == NULL) {
-		
-		
-		rms_map = HT_construct(NULL);
-		#if DEBUG
-		AP_ASSERT(rms_map != NULL, "sms_map not allocable");
-		#endif
-		
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(HT);
-		#endif
-		
-		rms_map->key = act->node->context_id;
-		HT_add_node(rtn_info->context_rms_map, rms_map->key, rms_map);
-
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(HTN);
-		#endif
-
-	} else {
-		
-		info_access = HT_lookup(rms_map, act->rms);
-	
-	}
-	#else
-	
-	info_access = HT_lookup(rtn_info->rms_map, act->rms);
-	
-	#endif
-	
-	// make new unique RMS entry
-	if (info_access == NULL) {
-		
-		//VG_(printf)("New sms info\n");
-		info_access = (RMSInfo * ) VG_(calloc)("sms_info", 1, sizeof(RMSInfo));
-		#if DEBUG
-		AP_ASSERT(info_access != NULL, "rms_info not allocable in function exit");
-		#endif
-		
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(RMS);
-		#endif
-		
-		// init minimum cumulative time for sms entry
-		info_access->min_cumulative_time = (ULong)-1;
-		info_access->self_time_min = (ULong)-1;
+    ULong partial_cumulative = start - act->entry_time;
+    
+    #if DISCARD_UNKNOWN
+    if (!rtn_info->fn->discard_info) {
+    #endif
+    
+    // check if routine has ever been called with this RMS
+    RMSInfo * info_access = NULL;
+    #if CCT
+    HashTable * rms_map  = HT_lookup(rtn_info->context_rms_map, 
+                                            act->node->context_id);
+    
+    if (rms_map == NULL) {
         
-		info_access->key = act->rms;
-		#if CCT
-		HT_add_node(rms_map, info_access->key, info_access);
-		
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(HTN);
-		#endif
-		
-		#else
-		HT_add_node(rtn_info->rms_map, info_access->key, info_access);
-		
-		#if DEBUG_ALLOCATION
-		APROF_(add_alloc)(HTN);
-		#endif
-		
-		#endif
+        
+        rms_map = HT_construct(NULL);
+        #if DEBUG
+        AP_ASSERT(rms_map != NULL, "sms_map not allocable");
+        #endif
+        
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(HT);
+        #endif
+        
+        rms_map->key = act->node->context_id;
+        HT_add_node(rtn_info->context_rms_map, rms_map->key, rms_map);
 
-	}
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(HTN);
+        #endif
 
-	// bookkeeping
-	
-	info_access->calls_number++;
-	
-	/* cumulative cost */
-	info_access->cumulative_time_sum += partial_cumulative;
-	info_access->cumulative_sum_sqr += (partial_cumulative*partial_cumulative);
-	
-	if (info_access->max_cumulative_time < partial_cumulative) 
-		info_access->max_cumulative_time = partial_cumulative;
-	
-	if (info_access->min_cumulative_time > partial_cumulative) 
-		info_access->min_cumulative_time = partial_cumulative;
-	
-	/* real cumulative cost */
-	if (rtn_info->recursion_pending < 2) 
-		info_access->cumul_real_time_sum += partial_cumulative;
+    } else {
+        
+        info_access = HT_lookup(rms_map, act->rms);
+    
+    }
+    #else
+    
+    info_access = HT_lookup(rtn_info->rms_map, act->rms);
+    
+    #endif
+    
+    // make new unique RMS entry
+    if (info_access == NULL) {
+        
+        //VG_(printf)("New sms info\n");
+        info_access = (RMSInfo * ) VG_(calloc)("sms_info", 1, sizeof(RMSInfo));
+        #if DEBUG
+        AP_ASSERT(info_access != NULL, "rms_info not allocable in function exit");
+        #endif
+        
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(RMS);
+        #endif
+        
+        // init minimum cumulative time for sms entry
+        info_access->min_cumulative_time = (ULong)-1;
+        info_access->self_time_min = (ULong)-1;
+        
+        info_access->key = act->rms;
+        #if CCT
+        HT_add_node(rms_map, info_access->key, info_access);
+        
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(HTN);
+        #endif
+        
+        #else
+        HT_add_node(rtn_info->rms_map, info_access->key, info_access);
+        
+        #if DEBUG_ALLOCATION
+        APROF_(add_alloc)(HTN);
+        #endif
+        
+        #endif
 
-	/* self cost */
-	ULong partial_self = partial_cumulative - act->total_children_time;
-	info_access->self_time_sum += partial_self;
-	info_access->self_sum_sqr += (partial_self*partial_self);
-	
-	if (info_access->self_time_max < partial_self) 
-		info_access->self_time_max = partial_self;
-	
-	if (info_access->self_time_min > partial_self) 
-		info_access->self_time_min = partial_self;
-	
-	#if DEBUG
-	AP_ASSERT(info_access->max_cumulative_time >= info_access->min_cumulative_time, "Min max mismatch");
-	if (info_access->calls_number == 1)
-		AP_ASSERT(info_access->min_cumulative_time == info_access->max_cumulative_time, "Min max mismatch");
-	#endif
-	
-	#if DISCARD_UNKNOWN
-	}
-	#endif
-	
-	rtn_info->recursion_pending--;
-	
+    }
 
-	// merge accesses of current activation with those of the parent activation
-	if (tdata->stack_depth > 1) {
+    // bookkeeping
+    
+    info_access->calls_number++;
+    
+    /* cumulative cost */
+    info_access->cumulative_time_sum += partial_cumulative;
+    info_access->cumulative_sum_sqr += (partial_cumulative*partial_cumulative);
+    
+    if (info_access->max_cumulative_time < partial_cumulative) 
+        info_access->max_cumulative_time = partial_cumulative;
+    
+    if (info_access->min_cumulative_time > partial_cumulative) 
+        info_access->min_cumulative_time = partial_cumulative;
+    
+    /* real cumulative cost */
+    if (rtn_info->recursion_pending < 2) 
+        info_access->cumul_real_time_sum += partial_cumulative;
 
-		Activation * parent_activation = APROF_(get_activation)(tdata, tdata->stack_depth - 1);
-		#if DEBUG
-		AP_ASSERT(parent_activation != NULL, "Invalid parent activation");
-		#endif
+    /* self cost */
+    ULong partial_self = partial_cumulative - act->total_children_time;
+    info_access->self_time_sum += partial_self;
+    info_access->self_sum_sqr += (partial_self*partial_self);
+    
+    if (info_access->self_time_max < partial_self) 
+        info_access->self_time_max = partial_self;
+    
+    if (info_access->self_time_min > partial_self) 
+        info_access->self_time_min = partial_self;
+    
+    #if DEBUG
+    AP_ASSERT(info_access->max_cumulative_time >= 
+        info_access->min_cumulative_time, "Min max mismatch");
+    if (info_access->calls_number == 1)
+        AP_ASSERT(info_access->min_cumulative_time == 
+        info_access->max_cumulative_time, "Min max mismatch");
+    #endif
+    
+    #if DISCARD_UNKNOWN
+    }
+    #endif
+    
+    rtn_info->recursion_pending--;
+    
 
-		#if TRACE_FUNCTION && IGNORE_DL_RUNTIME
-		if (!tdata->skip) {
-		#endif
-		
-			parent_activation->rms                 += act->rms;
-			parent_activation->total_children_time += partial_cumulative;
-		
-		#if TRACE_FUNCTION && IGNORE_DL_RUNTIME
-		} else {
-	
-			#if TIME == BB_COUNT 
-			if (act->skip)
-				tdata->bb_c -= partial_cumulative;
-			#else
-	
-			AP_ASSERT(0, "With RDTSC you can't ignore dl_runtime_resolve");
-			#endif
-		}
-		#endif
-	
-	}
-	
+    // merge accesses of current activation with those of the parent activation
+    if (tdata->stack_depth > 1) {
 
-	#if TRACE_FUNCTION && IGNORE_DL_RUNTIME 
-	if (act->skip) tdata->skip = False;
-	#endif
+        Activation * parent_activation = APROF_(get_activation)(
+                                        tdata, tdata->stack_depth - 1);
+        #if DEBUG
+        AP_ASSERT(parent_activation != NULL, "Invalid parent activation");
+        #endif
+
+        #if TRACE_FUNCTION && IGNORE_DL_RUNTIME
+        if (!tdata->skip) {
+        #endif
+        
+            parent_activation->rms                 += act->rms;
+            parent_activation->total_children_time += partial_cumulative;
+        
+        #if TRACE_FUNCTION && IGNORE_DL_RUNTIME
+        } else {
+    
+            #if TIME == BB_COUNT 
+            if (act->skip)
+                tdata->bb_c -= partial_cumulative;
+            #else
+    
+            AP_ASSERT(0, "With RDTSC you can't ignore dl_runtime_resolve");
+            #endif
+        }
+        #endif
+    
+    }
+    
+
+    #if TRACE_FUNCTION && IGNORE_DL_RUNTIME 
+    if (act->skip) tdata->skip = False;
+    #endif
 
 }
--- valgrind/aprof/fwrite.c
+++ valgrind/aprof/fwrite.c
@@ -1,5 +1,5 @@
 /*
- * Aprof implementation of libc fopen, fwrite, fclose, fflush functions 
+ * aprof's implementation of libc fopen, fwrite, fclose, fflush functions 
  * 
  * Last changed: $Date$
  * Revision:     $Rev$
@@ -10,7 +10,8 @@
 
    Copyright (C) 2011-2012, Emilio Coppa (ercoppa@gmail.com),
                             Camil Demetrescu,
-                            Irene Finocchi
+                            Irene Finocchi,
+                            Romolo Marotta
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -32,54 +33,54 @@
 
 #include "aprof.h"
 
-/* Note: fwrite() & co is not provided by valgrind, so... */
+/* Note: fwrite() & co are not provided by valgrind, so... */
 
 FILE * APROF_(fopen)(char * name){
-	
-	SysRes res = VG_(open)(name, VKI_O_EXCL|VKI_O_CREAT|VKI_O_WRONLY, VKI_S_IRUSR|VKI_S_IWUSR);
-	int file = (Int) sr_Res(res);
-	if (file <= 0) return NULL;
-	
-	//AP_ASSERT(file >= 0, "Can't create a log file.")
-	
-	FILE * f = VG_(malloc)("log_file", sizeof(FILE));
-	f->file = file;
-	f->fw_pos = 0;
-	
-	return f;
+    
+    SysRes res = VG_(open)(name, VKI_O_EXCL|VKI_O_CREAT|VKI_O_WRONLY, VKI_S_IRUSR|VKI_S_IWUSR);
+    int file = (Int) sr_Res(res);
+    if (file <= 0) return NULL;
+    
+    //AP_ASSERT(file >= 0, "Can't create a log file.")
+    
+    FILE * f = VG_(malloc)("log_file", sizeof(FILE));
+    f->file = file;
+    f->fw_pos = 0;
+    
+    return f;
 
 }
 
 void APROF_(fflush)(FILE * f) {
-	
-	unsigned int bw = 0, bf = 0, size = f->fw_pos;
-	do {
-		bf = VG_(write)(f->file, f->fw_buffer + bw, size - bw);
-		AP_ASSERT(bf != -1, "Error during writing\n");
-		bw += bf;
-	} while(bw < size);
-	
-	f->fw_pos = 0;
+    
+    unsigned int bw = 0, bf = 0, size = f->fw_pos;
+    do {
+        bf = VG_(write)(f->file, f->fw_buffer + bw, size - bw);
+        AP_ASSERT(bf != -1, "Error during writing\n");
+        bw += bf;
+    } while(bw < size);
+    
+    f->fw_pos = 0;
 
 }
 
 void APROF_(fwrite)(FILE * f, char * buffer, unsigned int size) {
 
-	if (buffer == NULL || size == 0) return;
-	
-	/* We suppose size always less than BUFFER_SIZE */
-	AP_ASSERT(size < BUFFER_SIZE, "Error during writing\n");
-	
-	if (BUFFER_SIZE - f->fw_pos <= size) APROF_(fflush)(f);
-	VG_(strncpy)(f->fw_buffer + f->fw_pos, buffer, size);
-	f->fw_pos += size;
+    if (buffer == NULL || size == 0) return;
+    
+    /* We suppose size always less than BUFFER_SIZE */
+    AP_ASSERT(size < BUFFER_SIZE, "Error during writing\n");
+    
+    if (BUFFER_SIZE - f->fw_pos <= size) APROF_(fflush)(f);
+    VG_(strncpy)(f->fw_buffer + f->fw_pos, buffer, size);
+    f->fw_pos += size;
 
 }
 
 void APROF_(fclose)(FILE * f) {
-	
-	APROF_(fflush)(f);
-	VG_(close)(f->file);
-	VG_(free)(f);
+    
+    APROF_(fflush)(f);
+    VG_(close)(f->file);
+    VG_(free)(f);
 
 }
