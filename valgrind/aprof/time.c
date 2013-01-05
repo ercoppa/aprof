eace5c40e1af1576c76d4d09838c94738ffe08a8
ercoppa <ercoppa@gmail.com>
2013-01-05 12:29:07 +0000

    [aprof:dev] merged external I/O on time.c

--- valgrind/aprof/time.c
+++ valgrind/aprof/time.c
@@ -8,9 +8,10 @@
  /*
    This file is part of aprof, an input sensitive profiler.
 
-   Copyright (C) 2011-2012, Emilio Coppa (ercoppa@gmail.com),
+   Copyright (C) 2011-2013, Emilio Coppa (ercoppa@gmail.com),
                             Camil Demetrescu,
-                            Irene Finocchi
+                            Irene Finocchi,
+                            Romolo Marotta
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -34,43 +35,35 @@
 
 ULong APROF_(time)(ThreadData * tdata) {
 
-	#if EMPTY_ANALYSIS
-	return 0;
-	#endif
+    #if EMPTY_ANALYSIS
+    return 0;
+    #endif
 
-	#if TIME == INSTR
-	return tdata->instr;
+    #if TIME == INSTR
+    return tdata->instr;
 
-	#elif TIME == RDTSC
-	ULong ret;
-	__asm__ __volatile__("rdtsc": "=A" (ret));
-	return ret;
+    #elif TIME == RDTSC
+    ULong ret;
+    __asm__ __volatile__("rdtsc": "=A" (ret));
+    return ret;
 
-	#elif TIME == BB_COUNT
-	return tdata->bb_c;
-	#endif
+    #elif TIME == BB_COUNT
+    return tdata->bb_c;
+    #endif
 
-	return 0;
+    return 0;
 
 }
-
-/*
-#if COUNT_VEX_I
-static VG_REGPARM(0) void add_one_IRStmt(void) {
-   vex_i++;
-}
-#endif
-*/
 
 #if TIME == INSTR
 VG_REGPARM(0) void APROF_(add_one_guest_instr)(void) {
-	APROF_(current_tdata)->instr++;
+    APROF_(current_tdata)->instr++;
 }
 #endif
 
 #if TIME == BB_COUNT && !TRACE_FUNCTION
 VG_REGPARM(0) void APROF_(add_one_guest_BB)(void) {
-	APROF_(current_tdata)->bb_c++;
+    APROF_(current_tdata)->bb_c++;
 }
 #endif
 
