726add0675c10248e851c662ea8efaa5ceabea55
Luca.DMaio@gmail.com <Luca.DMaio@gmail.com@7c8bc7a8-f605-be24-c186-4ff6029ff6b6>
2014-01-04 17:28:41 +0000

    Reduced images resolution, function for minified .eps cost funct

--- trunk/pyaprofplot/functionSet.py
+++ trunk/pyaprofplot/functionSet.py
@@ -7,135 +7,72 @@ import numpy as np
 import sys as sys
 import os
 import math
-import fitting as fit
-import matplotlib.gridspec as gridspec
 
 cmap = plt.cm.jet
 bounds=[-1,0,1,4,8,12,16,32,128,256,1024,2048,4000,sys.maxint-1,sys.maxint]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 newBounds=[1,4,8,12,16,32,128,256,1024,1800,6500]
 
-def create_grid(f,functL):
-	fig = plt.figure()
-	functN=len(functL)
-	r=0
-	c=0
-	v=0
-	retval = os.getcwd()
-	os.chdir("tmp")
-	'''adjusting factor for mam legend'''
-	global mamAdjust
-	if functN==1 or functN==2:
-		mamAdjust=-0.13
-	elif functN==3 or functN==4:
-		mamAdjust=-0.32
-	elif functN==5:
-		mamAdjust=-0.50
-	''''''
-	if functN==1:
-		gs = gridspec.GridSpec(1, 2)
-	elif functN%2!=0:
-		gs = gridspec.GridSpec((functN+1)/2, 2)
-	else:
-		gs = gridspec.GridSpec(functN/2, 2)
-	gs.update(wspace=0.25,hspace=0.5)
-
-	if functN%2!=0:
-		globals()[functL[0]](f,fig.add_subplot(gs[0,:]))
-		r=1
-		v=1
-	for i in functL[v:]:
-		globals()[i](f,fig.add_subplot(gs[r,c]))
-		if c==1:
-			c=0
-			r+=1
-		else:
-			c+=1
-
-
-	fig.savefig(f.rName+".eps",bbox_inches="tight",dpi=400)
-	os.chdir(retval)
-
-	
-def costPlot(funct,pl):
+def costPlot(funct):
 	x=np.array([])
 	y=np.array([])
 	f=np.array([])
 	x=np.append(x,funct.rmsSet[:,0])
 	y=np.append(y,funct.rmsSet[:,2])
 	f=np.append(f,funct.rmsSet[:,5])
-	pl.axis([0, np.amax(x)+(2*np.amax(x)/100), 0, np.amax(y)+(2*np.amax(y)/100)])
-	pl.set_xlabel('read memory size',fontsize=9)
-	pl.set_ylabel("cost",fontsize=9)
-	pl.grid(True)
-	pl.set_title("Cost")
-	pl.tick_params(axis='x', labelsize=7)
-	pl.tick_params(axis='y', labelsize=7)
-	sc=pl.scatter(x[1:],y[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
-	pylab.close()
-	cbar=pylab.colorbar(sc,cmap=cmap, norm=norm, boundaries=newBounds, ticks=[],aspect=20,shrink=0.0,pad=0.00)
+	pylab.axis([0, np.amax(x)+(10*np.amax(x)/100), 0, np.amax(y)+(10*np.amax(y)/100)])
+	pylab.xlabel('read memory size',fontsize=15)
+	pylab.ylabel("cost",fontsize=15)
+	pylab.grid(True)
+	sc=pylab.scatter(x[1:],y[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
+	cbar=pylab.colorbar(sc,cmap=cmap, norm=norm, boundaries=newBounds, ticks=[1, 16,256,4000],aspect=20,shrink=0.4)
+	cbar.set_label("RMS frequency",size=15)
 	"""plt.show()"""
+	fig=file(os.path.join("tmp",funct.rName+'_cost.eps'),'wb')
+	pylab.savefig(fig,format='eps')
+	fig.close()
+	pylab.close()
+	return "cost"
 	
-def mamPlot(funct,pl):
-	x=np.array([])
-	ymin=np.array([])
-	yavg=np.array([])
-	ymax=np.array([])
-	f=np.array([])
-	x=np.append(x,funct.rmsSet[:,0])
-	ymin=np.append(ymin,funct.rmsSet[:,1])
-	ymax=np.append(ymax,funct.rmsSet[:,2])
-	t1=funct.rmsSet[:,3]
-	t2=funct.rmsSet[:,5]
-	yavg=np.append(yavg,t1[1:]/t2[1:])
-	f=np.append(f,funct.rmsSet[:,5])
-	pl.axis([0, np.amax(x)+(2*np.amax(x)/100), 0, np.amax(ymax)+(2*np.amax(ymax)/100)])
-	pl.set_xlabel('read memory size',fontsize=9)
-	pl.set_ylabel("cost",fontsize=9)
-	pl.grid(True)
-	pl.set_title("Min/Avg/Max Cost")
-	pl.tick_params(axis='x', labelsize=7)
-	pl.tick_params(axis='y', labelsize=7)
-	sc=pl.scatter(x[1:],ymax[1:],s=7,c='r', marker = 's',lw=0.0)
-	sc1=pl.scatter(x[1:],yavg,s=6.5,c='g', marker = 's',lw=0.0)	
-	sc2=pl.scatter(x[1:],ymin[1:],s=6,c='b', marker = 's',lw=0.0)	
-	pl.legend((sc2,sc1,sc),("Min","Avg","Max"),scatterpoints=1,ncol=3,bbox_to_anchor=[0.5, mamAdjust],loc="lower center",fontsize=8)
-	
-def RMSFreqPlot(funct,pl):
+def RMSFreqPlot(funct):
 	x=np.array([])
 	y=np.array([])
 	f=np.array([])
 	x=np.append(x,funct.rmsSet[:,0])
 	y=np.append(f,funct.rmsSet[:,5])
-	pl.axis([-2, np.amax(x)+(2*np.amax(x)/100), 0, np.amax(y)+(2*np.amax(y)/100)])
-	pl.set_xlabel("read memory size",fontsize=9)
-	pl.set_ylabel("frequency",fontsize=9)
-	pl.set_title("RMS Frequency")
-
-	pl.tick_params(axis='x', labelsize=7)
-	pl.tick_params(axis='y', labelsize=7)
-	pl.grid(True)
-	sc=pl.scatter(x[1:],y[1:],s=6, marker = 's',lw=0.0)
+	pylab.axis([0, np.amax(x)+(10*np.amax(x)/100), 0, np.amax(y)+(10*np.amax(y)/100)])
+	pylab.xlabel("read memory size",fontsize=15)
+	pylab.ylabel("frequency",fontsize=15)
+	pylab.grid(True)
+	sc=pylab.scatter(x[1:],y[1:],s=6, marker = 's',lw=0.0)
+	fig=file(os.path.join("tmp",funct.rName+'_RMSFreq.eps'),'wb')
+	pylab.savefig(fig,format='eps')
+	fig.close()
+	pylab.close()
+	return "RMSFreq"
 	
-def TotalCostPlot(funct,pl):
+def TotalCostPlot(funct):
 	x=np.array([])
 	y=np.array([])
 	f=np.array([])
 	x=np.append(x,funct.rmsSet[:,0])
 	y=np.append(y,funct.rmsSet[:,3])
 	f=np.append(f,funct.rmsSet[:,5])
-	pl.axis([0, np.amax(x)+(2*np.amax(x)/100), 0, np.amax(y)+(2*np.amax(y)/100)])
-	pl.set_xlabel("read memory size",fontsize=9)
-	pl.set_ylabel("cost",fontsize=9)
-	pl.grid(True)
-	pl.set_title("Total Cost")
-	pl.tick_params(axis='x', labelsize=7)
-	pl.tick_params(axis='y', labelsize=7)
-	sc=pl.scatter(x[1:],y[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
+	pylab.axis([0, np.amax(x)+(10*np.amax(x)/100), 0, np.amax(y)+(10*np.amax(y)/100)])
+	pylab.xlabel("read memory size",fontsize=15)
+	pylab.ylabel("cost",fontsize=15)
+	pylab.grid(True)
+	sc=pylab.scatter(x[1:],y[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
+	cbar=pylab.colorbar(sc,cmap=cmap, norm=norm, boundaries=newBounds, ticks=[1, 16,256,4000],aspect=20,shrink=0.4)
+	cbar.set_label("RMS frequency",size=15)
+	"""plt.show()"""
+	fig=file(os.path.join("tmp",funct.rName+'_TotCost.eps'),'wb')
+	pylab.savefig(fig,format='eps')
+	fig.close()
 	pylab.close()
-	cbar=pylab.colorbar(sc,cmap=cmap, norm=norm, boundaries=newBounds, ticks=[],aspect=20,shrink=0.0,pad=0.00)
+	return "TotCost"
 	
-def curveBounding(funct,B,pl):
+def curveBounding(funct,B):
 	x=np.array([])
 	y=np.array([])
 	f=np.array([])
@@ -152,39 +89,16 @@ def curveBounding(funct,B,pl):
 		else:
 			 z=np.append(z,[0])
 		i+=1
-	pl.axis([0, np.amax(x)+(2*np.amax(x)/100), 0, np.amax(z)+(2*np.amax(z)/100)])
-	pl.xlabel('read memory size',fontsize=9)
-	pl.ylabel("ratio",fontsize=9)
-	pl.grid(True)
-	sc=pl.scatter(x[1:],z[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
-	pylab.close()
+	pylab.axis([0, np.amax(x)+(10*np.amax(x)/100), 0, np.amax(z)+(10*np.amax(z)/100)])
+	pylab.xlabel('read memory size',fontsize=15)
+	pylab.ylabel("ratio",fontsize=15)
+	pylab.grid(True)
+	sc=pylab.scatter(x[1:],z[1:],s=6,c=f[1:], marker = 's',cmap=cmap,norm=norm,lw=0.0)
 	cbar=pylab.colorbar(sc,cmap=cmap, norm=norm, boundaries=newBounds, ticks=[1, 16,256,4000],aspect=20,shrink=0.4)
 	cbar.set_label("RMS frequency",fontsize=15)
-	pl.title("T(n)/nlog(n)",fontsize=20)
-
-def MeanCumulativePlot(funct,pl):
-	x=np.array([])
-	y=np.array([])
-	f=np.array([])
-	z=np.array([])
-	x=np.append(x,funct.rmsSet[:,0])
-	y=np.append(y,funct.rmsSet[:,3])
-	f=np.append(f,funct.rmsSet[:,5])
-	z=np.append(z,[0])
-	num=0
-	den=0
-	i=1
-	while i<len(x):
-		num+=y[i]
-		den+=f[i]
-		z=np.append(z,[num/den])
-		i+=1
-	pl.axis([0, np.amax(x)+(10*np.amax(x)/100), 0, np.amax(z)+(10*np.amax(z)/100)])
-	pl.set_xlabel("read memory size",fontsize=9)
-	pl.set_ylabel("mean cumulative cost",fontsize=9)
-	pl.set_title("Mean Cumulative Cost")
-	pl.grid(True)
-	pl.tick_params(axis='x', labelsize=7)
-	pl.tick_params(axis='y', labelsize=7)
-	sc=pl.scatter(x[1:],z[1:],s=6,marker = 's',lw=0.0)
-	"""plt.show()"""
+	pylab.title("T(n)/nlog(n)",fontsize=20)	
+	fig=file(os.path.join("tmp",funct.rName+'_curve_Bounding.eps'),'wb')
+	pylab.savefig(fig,format='eps')
+	fig.close()
+	pylab.close()
+	return "curve_Bounding"
