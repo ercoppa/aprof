5388c0671410e094b5b59e054e6c7d2387d8d154
Luca.DMaio@gmail.com <Luca.DMaio@gmail.com@7c8bc7a8-f605-be24-c186-4ff6029ff6b6>
2014-01-04 17:26:22 +0000

    Added routines properties

--- trunk/pyaprofplot/AFunct.py
+++ trunk/pyaprofplot/AFunct.py
@@ -2,21 +2,27 @@ import numpy as np
 from operator import itemgetter
 
 class AFunct: 
-	def __init__(self, rName, imName, id): 
+	def __init__(self, rName, imName, fid,libName): 
 		self.rName=rName
 		self.imName=imName
-		self.id=id
+		self.fid=fid
 		self.rmsSet=np.array([[0,0,0,0,0,0,0,0,0,0,0]])
-		self.totCount=0
-		self.Nrms=0;
+		self.totCalls=0
+		self.totCallsPerc=0
+		self.cumulativeCost=0
+		self.cumulativeCostPerc=0
+		self.Nrms=0
+		self.lib=libName
 	def addRms(self, rms):
 		rms=map(int,rms[2:])
 		self.rmsSet=np.vstack([self.rmsSet,rms])
-		self.totCount+=int(rms[7])
+		self.totCalls+=rms[5]
+		self.cumulativeCost+=rms[6]
 		self.Nrms+=1
 	def getN(self):
 		self.rName
 	def reorder(self):
 		temp=self.rmsSet[:,0].argsort()
 		self.rmsSet=self.rmsSet[temp]
+
 
