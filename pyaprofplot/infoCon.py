6b33c147e16e972665b0060af1dd07ee015038d2
Luca.DMaio@gmail.com <Luca.DMaio@gmail.com@7c8bc7a8-f605-be24-c186-4ff6029ff6b6>
2014-01-04 17:30:11 +0000

    Added some properties-calculation for routines

--- pyaprofplot/infoCon.py
+++ pyaprofplot/infoCon.py
@@ -1,11 +1,12 @@
 class infoCon:
-	def __init__(self,vN,k,ex,m,tC):
+	def __init__(self,vN,k,ex,m,tCalls):
 		self.versionNumber=vN
-		self.totalCost=k
+		self.totalCumulCost=k
 		self.execName=ex
 		self.metric=m
 		self.functContainer=[]
-		self.totCount=tC	
+		self.totFunctsCalls=tCalls
 	def addFunct(self,funct):
 		self.functContainer.append(funct)
-		
+		funct.totCallsPerc=round(100*float(funct.totCalls)/self.totFunctsCalls,2)
+		funct.cumulativeCostPerc=round(100*float(funct.cumulativeCost)/self.totalCumulCost,2)
