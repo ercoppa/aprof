2cdc9ef57b5127fb38bb6360d877fa55699e61f4
Luca.DMaio@gmail.com <Luca.DMaio@gmail.com@7c8bc7a8-f605-be24-c186-4ff6029ff6b6>
2014-03-09 21:16:03 +0000

    Refactoring

--- trunk/pyaprofplot/infoCon.py
+++ trunk/pyaprofplot/infoCon.py
@@ -1,3 +1,4 @@
+import numpy as np
 class infoCon:
 	def __init__(self,vN,k,ex,m,tCalls):
 		self.versionNumber=vN
@@ -8,5 +9,6 @@ class infoCon:
 		self.totFunctsCalls=tCalls
 	def addFunct(self,funct):
 		self.functContainer.append(funct)
-		funct.totCallsPerc=round(100*float(funct.totCalls)/self.totFunctsCalls,2)
+		funct.totCallsPerc=round(100*float(funct.totCalls)/self.totFunctsCalls,3)
 		funct.cumulativeCostPerc=round(100*float(funct.cumulativeCost)/self.totalCumulCost,2)
+		funct.rmsSet=np.delete(funct.rmsSet,0,0)#remove the first tuple (dummy)
